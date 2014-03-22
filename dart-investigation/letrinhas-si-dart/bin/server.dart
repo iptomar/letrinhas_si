7f6004b60a3d8d15a54c8e9c0b84953a5545ab74
André Carvalho <afecarvalho@gmail.com>
2014-03-22 22:15:04 +0000

    Formatação dos ficheiros.

--- dart-investigation/letrinhas-si-dart/bin/server.dart
+++ dart-investigation/letrinhas-si-dart/bin/server.dart
@@ -8,7 +8,7 @@ void _fetchTests(_io.HttpRequest req) {
     req.response
         ..write(jsonData)
         ..close();
-    
+
     print('Sent results to the client.');
   });
 }
@@ -23,12 +23,12 @@ void _postResults(_io.HttpRequest req) {
 void _handleRequest(_io.HttpRequest req) {
   // First, don't forget to add CORS headers, or the request will fail.
   _addCorsHeaders(req.response);
-  
+
   print('Got a ${req.method} request to ${req.uri.path}.');
-  
+
   if (req.method == 'POST') {
     // Handle POST requests.
-    
+
     switch (req.requestedUri.pathSegments[0]) {
       case 'postTestResults':
         _postResults(req);
@@ -38,7 +38,7 @@ void _handleRequest(_io.HttpRequest req) {
     }
   } else if (req.method == 'GET') {
     // Handle GET requests.
-    
+
     switch (req.requestedUri.pathSegments[0]) {
       case 'testList':
         _fetchTests(req);
@@ -62,12 +62,13 @@ void startServer() {
   _io.HttpServer.bind('0.0.0.0', 8888).then((server) {
     server.listen((req) => _handleRequest(req));
   });
-  
+
   print('Server running on 0.0.0.0:8888');
 }
 
 void _addCorsHeaders(_io.HttpResponse res) {
   res.headers.add("Access-Control-Allow-Origin", "*, ");
   res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.headers.add("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
 }
