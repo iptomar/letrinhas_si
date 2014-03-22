ac0eba9c6ab48ca37ff856b24046d95bcdf2b727
André Carvalho <afecarvalho@gmail.com>
2014-03-22 21:47:50 +0000

    Criação do WebService em Dart
    Criação de um WebService de suporte à app Android, feito em Dart. Para
    fins experimentais.

---
+++ dart-investigation/letrinhas-si-dart/bin/asynctasks.dart
@@ -0,0 +1,79 @@
+library asynctasks;
+
+import 'package:sqljocky/sqljocky.dart';
+
+import 'dart:async';
+import 'dart:io' as _io;
+import 'dart:convert' as _convert;
+
+// Create a connection pool to handle DB queries.
+ConnectionPool _pool = new ConnectionPool(
+  host: '192.168.56.101',
+  user: 'psiapp',
+  password: 'psiapp',
+  db: 'letrinhas'
+);
+
+/// Fetches tests from a database, and returns in JSON form.
+Future<String> getTestsFromDb() {
+  Completer completer = new Completer();
+
+  // Query the DB.
+  _pool.query('SELECT * FROM letrinhas.Testes;').then((Results res) {
+    print('Got the results from the query!');
+        
+    // Prepare the structure which will be transformed to JSON.
+    var data = <String, dynamic>{
+      'tests': <Map<String, dynamic>>[]
+    };
+        
+    res.forEach((Row row) {
+      // For each row of the result, add the results to an object and
+      // add the object to the list.
+      data['tests'].add({
+        'id': row.id,
+        'title': row.title,
+        'text': row.textContent.toString(),
+        'professorName': row.professorName,
+        'maxTries': row.maxTries       
+      });
+    }).then((_) {
+      // Serialize the map to JSON and send it down the request.
+       completer.complete(_convert.JSON.encode(data));
+    },
+    onError: (ex) => print('Something went wrong: ${ex.toString()}'));    
+  });
+  
+  print('Querying the database...');
+  
+  return completer.future;
+}
+
+/// Receives a [json] string containing test resolution data,
+/// parses it, and inserts it into the DB.
+/// This is run asynchronously.
+Future saveResultsToDb(String json) {
+  return new Future(() {
+    // First, parse the text.
+    Map<String, dynamic> dataToStore = _convert.JSON.decode(json);
+    
+    // Get the test list.
+    List<Map<String, dynamic>> tests = dataToStore['solvedTests'];
+     
+    List<List> parameters = new List<List>();
+    
+    // Prepare an INSERT to the DB.
+    _pool.prepare('INSERT INTO letrinhas.Resolucoes VALUES (?, ?, ?, ?, ?)')
+    .then((query) {
+      // Add all the tests to the query.
+      for (var test in tests) {
+        parameters.add(test.values.toList(growable: false));
+      }
+      
+      // Run the query. If anything goes wrong, we'll tell as such.
+      query.executeMulti(parameters).catchError((ex) => print(ex.toString()));
+    }, 
+    // If something goes wrong while preparing the query, tell it here.
+    onError: (ex) => print('Unable to prepare query., ${ex.toString()}'));
+  });
+}
---
+++ dart-investigation/letrinhas-si-dart/bin/server.dart
@@ -0,0 +1,73 @@
+library server;
+
+import 'dart:io' as _io;
+import 'asynctasks.dart' as tasks;
+
+void _fetchTests(_io.HttpRequest req) {
+  tasks.getTestsFromDb().then((jsonData) {
+    req.response
+        ..write(jsonData)
+        ..close();
+    
+    print('Sent results to the client.');
+  });
+}
+
+void _postResults(_io.HttpRequest req) {
+  req.listen((List<int> data) {
+    tasks.saveResultsToDb(new String.fromCharCodes(data));
+    req.response.close();
+  });
+}
+
+void _handleRequest(_io.HttpRequest req) {
+  // First, don't forget to add CORS headers, or the request will fail.
+  _addCorsHeaders(req.response);
+  
+  print('Got a ${req.method} request to ${req.uri.path}.');
+  
+  if (req.method == 'POST') {
+    // Handle POST requests.
+    
+    switch (req.requestedUri.pathSegments[0]) {
+      case 'postTestResults':
+        _postResults(req);
+        break;
+      default:
+        _sendNotFound(req);
+    }
+  } else if (req.method == 'GET') {
+    // Handle GET requests.
+    
+    switch (req.requestedUri.pathSegments[0]) {
+      case 'testList':
+        _fetchTests(req);
+        break;
+      default:
+        _sendNotFound(req);
+    }
+  } else {
+    // If we got this far, it means something went wrong.
+    _sendNotFound(req);
+  }
+}
+
+void _sendNotFound(_io.HttpRequest req) {
+  print('Sending 404!');
+  req.response.statusCode = 404;
+  req.response.close();
+}
+
+void startServer() {
+  _io.HttpServer.bind('0.0.0.0', 8888).then((server) {
+    server.listen((req) => _handleRequest(req));
+  });
+  
+  print('Server running on 0.0.0.0:8888');
+}
+
+void _addCorsHeaders(_io.HttpResponse res) {
+  res.headers.add("Access-Control-Allow-Origin", "*, ");
+  res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
