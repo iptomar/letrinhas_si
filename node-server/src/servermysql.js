a858494b95f32c8de441638b5e03ed331c1d89ab
luisfmoliveira <luis_oliveira_@live.com.pt>
2014-03-27 20:15:00 +0000

    Versão intermédia do Node.js Server onde se faz uma http request do tipo GET e é executada uma query na base de dados do tipo SELECT. NOTA: Dados apresentados apenas na consola do Node no Brackets, ainda não está a converter para JSON

--- node-server/src/servermysql.js
+++ node-server/src/servermysql.js
@@ -1,38 +1,71 @@
 /*global require*/
 /*jshint devel: true*/
+/*jslint devel: true*/
+/*global require, module*/
 
 //-- Tarefas a realizar neste ficheiro:
 // criar modulo export
 //encapsular as pools, de modo a que quando o servidor recebe um get utilize a pool com a query de SELECT, quando recebe um post utilize a pool de insert
 //Nota: muito semelhante ao que esta no DART
 
+
 //cria conecção com o servidor
 var mysql = require('mysql');
 var pool  = mysql.createPool({
-  host     : '192.168.56.101',
-  user     : 'psiapp',
-  password : 'psiapp',
-  database : 'letrinhas'
+        host     : '192.168.56.101',
+        user     : 'psiapp',
+        password : 'psiapp',
+        database : 'letrinhas'
 });
+ 
 
-//Seleciona a base de dados
+module.exports = {
+    getResponse: function(response){
+        _fetchTests(response);
+    }    
+};
 
-pool.query('SELECT Nome as "Nome do Professor" from Professores as solution;' ,function(err, rows, fields) {
-  if (err) throw err;
+
+function _fetchTests(response){
+    
+    pool.query('SELECT Nome as "Nome do Professor" from Professores as solution;' ,function(err, rows, fields) {
+    if (err) throw err;
     
     //imprimir os registos da base de dados
     for (var i = 0; i < rows.length; i++) {
         console.log(rows[i]['Nome do Professor']);   
     }
 
-  // console.log('Professores na BD ', rows[0].solution);
-});
-
-/*INSERT TO BD*/
-pool.getConnection(function(err, connection) {
-    var professor  = {Id: 3, User: 'jc1990', Nome: 'Joni Correia'};
-    var query = connection.query('INSERT INTO Professores SET ?', professor, function(err, result) {
-        console.log(query.sql);
+   // console.log('Professores na BD ', rows[0].solution);
     });
-});
+
+}
+
+
+
+
+
+
+////Seleciona a base de dados
+//
+//pool.query('SELECT Nome as "Nome do Professor" from Professores as solution;' ,function(err, rows, fields) {
+//  if (err) throw err;
+//    
+//    //imprimir os registos da base de dados
+//    for (var i = 0; i < rows.length; i++) {
+//        console.log(rows[i]['Nome do Professor']);   
+//    }
+//
+//  // console.log('Professores na BD ', rows[0].solution);
+//});
+//
+////INSERT TO BD
+//pool.getConnection(function(err, connection) {
+//    var professor  = {Id: 3, User: 'jc1990', Nome: 'Joni Correia'};
+//    var query = connection.query('INSERT INTO Professores SET ?', professor, function(err, result) {
+//        console.log(query.sql);
+//    });
+//});
+
+
 
