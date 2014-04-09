084196d23c7ca6d71b64c2a47d8466eea3552464
André Carvalho <afecarvalho@gmail.com>
2014-04-09 23:54:28 +0100

    Movi o .gitignore para uma diretoria acima, para ignorar mesmo (sigh) os .suo. Portei o método que insere resoluções na DB para express (Not Done Yet).

--- Projetos Servidor/letrinhas-node/letrinhas-node/Scripts/services/appPostServices.ts
+++ Projetos Servidor/letrinhas-node/letrinhas-node/Scripts/services/appPostServices.ts
@@ -7,3 +7,23 @@ export function sendBinaryDataToDb(binaryData: NodeBuffer, onDone: (err: Error) 
         onDone(err);
     });
 }
+
+export function saveTestToDb(jsonData: string, onDone: (err: Error) => void) {
+    var testData = JSON.parse(jsonData);
+
+    var list: any[] = testData.solvedTests;
+
+    var insertData = [[]];
+
+    for (var i = 0; i < list.length; i++) {
+        insertData.push(
+            [/*list[0].id, */list[0].testId, list[0].completionDate, list[0].studentName, new Buffer(list[i].voiceBase64, 'base64')]
+        );
+    }
+
+    var sql = 'INSERT INTO Resolucoes (testId, completionDate, studentName, voiceData) VALUES (?, ?, ?, ?)';
+
+    var query = mysql.pool.query(sql, insertData, (err, result) => {
+        onDone(err);
+    });
+}
