b0a42a39eea6829475fda73c4a18073f1ad27136
jpcarvalho <jpjcarvalho89@gmail.com>
2014-05-13 13:39:41 +0100

    pequenas alterações.

--- Projetos Servidor/letrinhas-node/letrinhas-node/Scripts/structures/tests/TestType.js
+++ Projetos Servidor/letrinhas-node/letrinhas-node/Scripts/structures/tests/TestType.js
@@ -2,6 +2,8 @@
 (function (TestType) {
     TestType[TestType["read"] = 0] = "read";
     TestType[TestType["multimedia"] = 1] = "multimedia";
+    TestType[TestType["list"] = 2] = "list";
+    TestType[TestType["poem"] = 3] = "poem";
 })(TestType || (TestType = {}));
 ;
 module.exports = TestType;
--- Projetos Servidor/letrinhas-node/letrinhas-node/Scripts/structures/tests/TestType.ts
+++ Projetos Servidor/letrinhas-node/letrinhas-node/Scripts/structures/tests/TestType.ts
@@ -1,5 +1,7 @@
 ﻿enum TestType {
     read = 0,
-    multimedia = 1
+    multimedia = 1,
+    list = 2,
+    poem = 3
 };
 export = TestType;
